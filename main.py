1212b31e40b378439181ed40307bb4ab031507ab
rivques <github@rivques.dev>
2026-03-01 16:45:10 -0500

    import and partition data

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import pandas as pd
+
+def import_data():
+    family_membership = pd.read_csv("all_tro.members.txt")
+    proper_elements = pd.read_csv("proper_elements.csv")
+
 def main():
-    print("Hello from scicomp-p2-asteroids-river!")
+    import_data()
 
 
 if __name__ == "__main__":
