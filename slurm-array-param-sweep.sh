5839259cb5b68b69a150d7b17c60518a14a0fd9c
rivques <github@rivques.dev>
2026-03-08 20:03:15 -0400

    swap to a patched version of sklearn, and prep for hdbscan_norm sweep

--- slurm-array-param-sweep.sh
+++ slurm-array-param-sweep.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 #SBATCH -c 2
 #SBATCH -N 1
-#SBATCH --mem=120G
+#SBATCH --mem=14G
 #SBATCH -p cpu  # Partition
 #SBATCH -t 02:00:00  # Job time limit
 #SBATCH -o sweep-%j.out  # %j = job ID
-#SBATCH --array 1-20
+#SBATCH --array 1-100
 #SBATCH --mail-type=END,FAIL
 
 echo "SLURM_ATI: $SLURM_ARRAY_TASK_ID"
-PARAM_STEP='0.0006'
-export PARAM_PER_JOB='0.0012'
+PARAM_STEP='0.001'
+export PARAM_PER_JOB='0.002'
 PARAM_LOW=$(python3 -c "import os; print(float(os.getenv('PARAM_PER_JOB')) * float(os.getenv('SLURM_ARRAY_TASK_ID')))")
 PARAM_HIGH=$(python3 -c "import os; print(float(os.getenv('PARAM_PER_JOB')) * (float(os.getenv('SLURM_ARRAY_TASK_ID'))+1))")
-echo "Running DBSCAN_3Param_Norm with epsilon in [$PARAM_LOW, $PARAM_HIGH) and min_samples in [4, 10) on the training set..."
+echo "Running HDBSCAN_3Param_Norm with epsilon in [$PARAM_LOW, $PARAM_HIGH) and min_samples in [5, 20) on the training set..."
 module load py-uv/0.4.27
-uv run python -m steps.20_parameter_sweep --classifier dbscan_3param_norm --dataset all --param eps=$PARAM_LOW:$PARAM_HIGH:$PARAM_STEP --param min_samples=4:10:2
+uv run python -m steps.20_parameter_sweep --classifier hdbscan_3param_norm --dataset all --param cluster_selection_epsilon=$PARAM_LOW:$PARAM_HIGH:$PARAM_STEP --param min_samples=5:20:5
